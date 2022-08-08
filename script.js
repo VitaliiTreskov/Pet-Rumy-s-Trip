78c19e1e078d4b88ccbfc721e22fec96b0ae7e06
Vitalii Treskov <vtcaruit@gmail.com>
2022-08-08 17:35:32 -0300

    version with simple slider for GitHub

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     
+
+    //Tabs of About my self section
     //get acsess to html elements for tabs
     const tabs = document.querySelectorAll('.tabheader_item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
@@ -41,4 +43,56 @@ window.addEventListener('DOMContentLoaded', () => {
                 });
             }
           });
+
+
+  //slider of On our way section
+  let slideIndex = 1;
+  const slides = document.querySelectorAll('.offer_slide'),
+        prev = document.querySelector('.offer_slider-prev'),
+        next = document.querySelector('.offer_slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+        
+  showSlides(slideIndex);
+
+  if(slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if(n > slides.length) {
+      slideIndex = slides.length;
+    }
+
+    if(n < 1) {
+      slideIndex = 1;
+    }
+
+    slides.forEach((item) => (item.style.display = 'none'));
+    slides[slideIndex - 1].style.display = 'block';
+
+    if(slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  next.addEventListener('click', () => {
+    plusSlides(1);
+  });
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
+  });
+
+
 });
